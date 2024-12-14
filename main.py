f6786deddd546db47082fbdb38dae9e99cbcd775
Ahmed <ahmed.emad.edu@gmail.com>
2024-12-14 15:27:27 +0300

    Added the files

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
+
+    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        
+
+if __name__ == "__main__":
+    main()
